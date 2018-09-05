9b6a8aa1914a77b8e88e73a4e2fb0c78b7bfe29b
nooha mohammed <noohamohammed00@gmail.com>
2018-09-05 20:29:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ function overwriteBestCustomer(){
 }
 leastFavoriteCustomer = 'nm'
 function changeLeastFavoriteCustomer(){
-  leastFavoriteCustomer=leastFavoriteCustomer.toLowerCase  
+  leastFavoriteCustomer=leastFavoriteCustomer.toLowerCase()  
 }
